42b0d14aec91dd83b8a07c91bf399ea530760c9b
Elena <tchka777@gmail.com>
2020-11-08 22:11:03 +0300

    debug toolbar

--- historical_games_shop/historical_games_shop/settings.py
+++ historical_games_shop/historical_games_shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'z3_+8(vgucvw21mlsihlhp950kc!l)x!=k_u#in-5rns+)z47$'
 #
 ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'adminapp',
     'orderapp',
     'social_django',
+    'debug_toolbar',
+    'template_profiler_panel',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,34 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+if DEBUG:
+   def show_toolbar(request):
+       return True
+
+   DEBUG_TOOLBAR_CONFIG = {
+       'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+   }
+
+   DEBUG_TOOLBAR_PANELS = [
+       'debug_toolbar.panels.versions.VersionsPanel',
+       'debug_toolbar.panels.timer.TimerPanel',
+       'debug_toolbar.panels.settings.SettingsPanel',
+       'debug_toolbar.panels.headers.HeadersPanel',
+       'debug_toolbar.panels.request.RequestPanel',
+       'debug_toolbar.panels.sql.SQLPanel',
+       'debug_toolbar.panels.templates.TemplatesPanel',
+       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+       'debug_toolbar.panels.cache.CachePanel',
+       'debug_toolbar.panels.signals.SignalsPanel',
+       'debug_toolbar.panels.logging.LoggingPanel',
+       'debug_toolbar.panels.redirects.RedirectsPanel',
+       'debug_toolbar.panels.profiling.ProfilingPanel',
+       'template_profiler_panel.panels.template.TemplateProfilerPanel',
+   ]
+
 
 ROOT_URLCONF = 'historical_games_shop.urls'
 
--- historical_games_shop/historical_games_shop/urls.py
+++ historical_games_shop/historical_games_shop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls.static import static
@@ -37,3 +37,9 @@ handler404 = mainapp.not_found
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
+
